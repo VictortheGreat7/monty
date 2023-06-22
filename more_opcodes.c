3928f3523eca94508016dcb9830f265fa44cddb2
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-22 10:30:10 +0000

    C - Stacks, Queues - LIFO, FIFO: Task 5 - Check 1

---
+++ more_opcodes.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+* do_nothing - Does not do anything
+* @head: Double pointer to the head/front
+* @line_num: Line number
+*/
+void do_nothing(stack_t **head, unsigned int line_num)
+{
+	(void) line_num;
+	(void) head;
+}
+
